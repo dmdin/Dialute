75b04f5b2dd70f97809f767d3cb8b2c5a9a23c6b
Din Dmitriy <dmitriy1d01@gmail.com>
2022-04-25 23:59:45 +0300

    🚀 Trying to launch example

---
+++ example/src/index.ts
@@ -0,0 +1,11 @@
+import { Dialute, SberRequest } from 'dialute';
+
+
+function* script(r: SberRequest) {
+  while (true) {
+    yield 'Hello world from Dialute!';
+  }
+}
+
+const d = Dialute.fromEntrypoint(script as GeneratorFunction);
+d.start();
